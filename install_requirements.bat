67a1438c5aecbd5e1d1ccffcde2e27d03a9fd993
CROC <christian_rocher@HOTMAIL.COM>
2016-04-23 15:39:28 -0400

    Review footer + some fields

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
-explorer.exe https://www.python.org/ftp/python/3.5.1/python-3.5.1-amd64.exe
-python-3.5.1-amd64.exe
-pip install click
-pip install dotmap
-pip install xlsxwriter
+cd install
+wget --no-check-certificate https://www.python.org/ftp/python/3.5.1/python-3.5.1-amd64.exe
+REM python-3.5.1-amd64.exe
+python get-pip.py
+python -m pip install click
+python -m pip install dotmap
+python -m pip install xlsxwriter
+cd ..
 
