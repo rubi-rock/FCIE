76420fd80a962439179088cd2ecb516d1e35f719
CROC <christian_rocher@HOTMAIL.COM>
2016-04-23 15:41:29 -0400

    Windows Installer

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,6 +1,6 @@
 cd install
 wget --no-check-certificate https://www.python.org/ftp/python/3.5.1/python-3.5.1-amd64.exe
-REM python-3.5.1-amd64.exe
+python-3.5.1-amd64.exe
 python get-pip.py
 python -m pip install click
 python -m pip install dotmap
